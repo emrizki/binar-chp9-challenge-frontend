55dbc08b13a7deefa9f2932a97548d2dfbd451b3
Biandi Erdiputra <biandierdiputra@Biandis-MacBook-Pro-2.local>
2021-11-23 20:56:08 +0700

    switched from class to function

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,53 +1,60 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment, useState } from 'react'
 import axios from 'axios'
+import { useHistory } from 'react-router-dom'
 import NavigationBar from '../components/Navbar'
 import '../styles/Login.css'
 
-export default class Login extends Component {
+function Login () {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
 
-    handleSubmit = e => {
-        e.preventDefault()
+    const [message, setMessage] = useState('')
+    const history = useHistory()
 
-        const data = {
-            username: this.username,
-            password: this.password
+    const submit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post('http://localhost:3000/api/login', {
+                username: username,
+                password: password
+            })
+            history.push('/')
+        } catch(error) {
+            if(error.response){
+                setMessage(error.response.data.message)
+            }
         }
-        const apiUrl = process.env.REACT_APP_API_BASE_URL;
-        axios.post(`${apiUrl}/login`, data)
-            .then(res => {
-                console.log(res)
-            })
-            .catch(err => {
-                console.log(err)
-            })
     }
 
-    render () {
-        return (
-            <Fragment>
-                <NavigationBar/>
-                <div className="login-bg">
-                    <div className="container-sm">
-                        <br/>
-                        <h2 className="row justify-content-center">LOGIN</h2>
-                        <div className="row justify-content-center">
-                            <div className="col-md-5">
-                                <form onSubmit={this.handleSubmit}>
-                                    <label className="col-form-label">Username</label>
-                                    <input type="text" className="form-control" required
-                                        onChange={e => this.username = e.target.value} />
-                                    <br/>
-                                    <label className="col-form-label">Password</label>
-                                    <input type="password" className="form-control" required
-                                        onChange={e => this.password = e.target.value} />
-                                    <br/>
-                                    <button className="btn login-btn">Login</button>
-                                </form>
-                            </div>
+    return (
+        <Fragment>
+            <NavigationBar/>
+            <div className="login-bg">
+                <div className="container-sm">
+                    <br/>
+                    <h2 className="row justify-content-center">LOGIN</h2>
+                    <div className="row justify-content-center">
+                        <div className="col-md-5">
+                            <form onSubmit={submit}>
+                                <label className="col-form-label">Username</label>
+                                <input type="text" className="form-control" required
+                                    value={username} onChange={(e) => setUsername (e.target.value)}/>
+                                <br/>
+                                <label className="col-form-label">Password</label>
+                                <input type="password" className="form-control" required
+                                    value={password} onChange={(e) => setPassword (e.target.value)}/>
+                                <br/>
+                                <button className="btn login-btn">Login</button>
+                                <br/>
+                                <br/>
+                                <h5 className="text-center">{message}</h5>
+                            </form>
                         </div>
                     </div>
                 </div>
-            </Fragment>
-        )
-    }
+            </div>
+        </Fragment>
+    )
 }
+
+export default Login;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,67 +1,77 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment, useState } from 'react'
 import axios from 'axios'
+import { useHistory } from 'react-router-dom'
 import NavigationBar from '../components/Navbar'
 import '../styles/Register.css'
 
-export default class Register extends Component {
+function Register () {
+    const [firstname, setFirstName] = useState('')
+    const [lastname, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
 
-    handleSubmit = e => {
-        e.preventDefault()
-        
-        const data = {
-            first_name: this.firstName,
-            last_name: this.lastName,
-            email: this.email,
-            username: this.username,
-            password: this.password
+    const [message, setMessage] = useState('')
+    const history = useHistory()
+
+    const submit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post('http://localhost:3000/api/register', {
+                first_name: firstname,
+                last_name: lastname,
+                email: email,
+                username: username,
+                password: password
+            })
+            history.push('/login')
+        } catch(error) {
+            if(error.response){
+                setMessage(error.response.data.message)
+            }
         }
-        const apiUrl = process.env.REACT_APP_API_BASE_URL;
-        axios.post(`${apiUrl}/register`, data)
-            .then(res => {
-                console.log(res)
-            })
-            .catch(err => {
-                console.log(err)
-            })
     }
 
-    render() {
-        return (
-            <Fragment>
-                <NavigationBar/>
-                <div className="register-bg">
-                    <div className="container-sm">
-                        <h2 className="row justify-content-center">REGISTER</h2>
-                        <div className="row justify-content-center">
-                            <div className="col-md-5">
-                                <form onSubmit={this.handleSubmit}>
-                                    <label className="col-form-label">First Name*</label>
-                                    <input type="text" className="form-control" required
-                                        onChange={e => this.firstName = e.target.value} />
-                                    <br/>
-                                    <label className="col-form-label">Last Name</label>
-                                    <input type="text" className="form-control"
-                                        onChange={e => this.lastName = e.target.value} />
-                                    <br/>
-                                    <label className="col-form-label">Email*</label>
-                                    <input type="email" className="form-control" required
-                                        onChange={e => this.email = e.target.value} />
-                                    <br/>
-                                    <label className="col-form-label">Username*</label>
-                                    <input type="text" className="form-control" required
-                                        onChange={e => this.username = e.target.value} />
-                                    <br/>
-                                    <label className="col-form-label">Password*</label>
-                                    <input type="password" className="form-control" required
-                                        onChange={e => this.password = e.target.value} />
-                                    <br/>
-                                    <button className="btn register-btn">Sign Up</button>
-                                </form>
-                            </div>
+    return (
+        <Fragment>
+            <NavigationBar/>
+            <div className="register-bg">
+                <div className="container-sm">
+                    <h2 className="row justify-content-center">REGISTER</h2>
+                    <div className="row justify-content-center">
+                        <div className="col-md-5">
+                            <form onSubmit={submit}>
+                                <label className="col-form-label">First Name*</label>
+                                <input type="text" className="form-control" required
+                                    value={firstname} onChange={(e) => setFirstName(e.target.value)}/>
+                                <br/>
+                                <label className="col-form-label">Last Name</label>
+                                <input type="text" className="form-control"
+                                    value={lastname} onChange={(e) => setLastName(e.target.value)}/>
+                                <br/>
+                                <label className="col-form-label">Email*</label>
+                                <input type="email" className="form-control" required
+                                    value={email} onChange={(e) => setEmail(e.target.value)}/>
+                                <br/>
+                                <label className="col-form-label">Username*</label>
+                                <input type="text" className="form-control" required
+                                    value={username} onChange={(e) => setUsername(e.target.value)}/>
+                                <br/>
+                                <label className="col-form-label">Password*</label>
+                                <input type="password" className="form-control" required
+                                    value={password} onChange={(e) => setPassword(e.target.value)}/>
+                                <br/>
+                                <button className="btn register-btn">Sign Up</button>
+                                <br/>
+                                <br/>
+                                <h5 className="text-center">{message}</h5>
+                            </form>
                         </div>
                     </div>
                 </div>
-            </Fragment>
-        )
-    }
+            </div>
+        </Fragment>
+    )
 }
+
+export default Register;

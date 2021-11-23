d99240d12118f84823962afb1857276ee2b4d45b
almas615 <almasadlilwafi615@gmail.com>
2021-11-23 10:33:55 +0700

    tambah route landing page dan detail game

--- src/App.js
+++ src/App.js
@@ -5,16 +5,22 @@ import Game from './pages/Game';
 import Profile from './pages/Profile';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import LandingPage from './pages/LandingPage';
+import DetailGame from './pages/DetailGame';
 
 class App extends Component {
     render() {
         return (
             <Router>
                 <Switch>
+                    <Route exact path="/" component={LandingPage}/>
                     <Route exact path="/game" component={Game} />
                     <Route exact path="/profile/:username" component={Profile} />
                     <Route exact path="/login" component={Login}/>
                     <Route exact path="/register" component={Register}/>
+                    <Route exact path="/game/:id" component={DetailGame}/>
+
+
                 </Switch>
             </Router>
         );
